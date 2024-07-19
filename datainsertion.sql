b0d8d45f3a21eb60cdaa7d30b81bad554636482a
RafaelViolas <145346865+RafaelViolas@users.noreply.github.com>
2024-07-19 08:48:38 +0000

    Document in datainsertion.sql

--- datainsertion.sql
+++ datainsertion.sql
@@ -1,18 +1,22 @@
+-- Inserir valores para a tabela Motorista
 INSERT INTO `Transportadora`.`Motoristas`
 (`Nome`,`CartaCondução`,`Telefone`)
 VALUES
 ('John Doe',000000001,000000001);
 
+-- Inserir valores para a tabela Rotas
 INSERT INTO `Transportadora`.`Rotas`
 (`Origem`, `Destino`, `Distância`)
 VALUES
 ('SuperMercado','Home',15);
 
+-- Inserir valores para a tabela Veículos
 INSERT INTO `Transportadora`.`Veículos`
 (`Marca`, `Modelo`, `Ano`, `Matrícula`, `Capacidade`)
 VALUES
 ('Mercedes','Mercedes-Benz S-class',2024,'00AA00',5);
 
+-- Inserir valores para a tabela Entregas
 INSERT INTO `Transportadora`.`Entregas`
 (`VeículoID`,`MotoristaID`,`RotaID`,`DataEntrega`,`Estado`)
 VALUES
