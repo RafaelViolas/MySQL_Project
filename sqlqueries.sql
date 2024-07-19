64b458b4d8c80f4d8a237a891686d46a0a573bfc
RafaelViolas <145346865+RafaelViolas@users.noreply.github.com>
2024-07-19 08:52:54 +0000

    Document in sqlqueries.sql

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -1,15 +1,17 @@
+-- Listar todos os veículos disponíveis com as respetivas marcas e capacidade
 Select `Veículos`.ID, Marca, Modelo, Ano, `Matrícula`, Capacidade
 from Transportadora.`Veículos`
 join Entregas on `Veículos`.ID = Entregas.VeículoID
 where Entregas.Estado <> 0;
 
+-- listar as entregas realizadas num determinado período
 Select * 
 from Entregas
 where DataEntrega 
 between '2024-07-15 10:00:00' -- starting time
 and '2025-05-15 10:00:00'; -- ending time this is not interchangeable
 
--- determinado numero de entregas(?)
+-- Listar os motoristas que realizaram mais de um determinado numero de entregas
 DROP VIEW IF EXISTS NumeroDeEntregas;
 CREATE VIEW NumeroDeEntregas AS
 Select Motoristas.ID,
@@ -20,11 +22,12 @@ Select Motoristas.ID,
 From Motoristas
 join Entregas on Motoristas.ID = Entregas.MotoristaID
 group by Motoristas.ID,CartaCondução;
-
+-- parte 2 da listagem dos motoristas que realizaram mais do que determinado numero de entregas
 Select * 
 from NumeroDeEntregas
 where numerodeentregas > 0;
 
+-- Listar as Rotas para um determinado destino
 Select * 
 from Rotas
 where Destino = 'Home';
