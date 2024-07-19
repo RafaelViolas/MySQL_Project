5dd5c26920f5900c9a9e2ef1976b68ce681f6f11
RafaelViolas <145346865+RafaelViolas@users.noreply.github.com>
2024-07-19 08:55:31 +0000

    Document in sqlstoredprocedures.sql

--- sqlstoredprocedures.sql
+++ sqlstoredprocedures.sql
@@ -1,7 +1,7 @@
 -- stored procedures
 
+-- Registar uma nova Entrega
 DROP PROCEDURE IF EXISTS insert_into_entregas;
-
 DELIMITER $$
 CREATE PROCEDURE `insert_into_entregas`(in _VeiculoID int, in _MotoristaID int,in _RotaID int,in _DataEntrega datetime,in _Estado tinyint(1))
 BEGIN
@@ -11,11 +11,10 @@ VALUES
 (_VeiculoID, _MotoristaID, _RotaID, _DataEntrega, _Estado);
 END$$
 DELIMITER ;
-
 CALL insert_into_entregas(2,2,1,"2024-07-15 14:20:00",0);
 
+-- Atualizar os dados de um motorista utilizando o seu ID
 DROP PROCEDURE IF EXISTS update_into_motoristas;
-
 DELIMITER $$
 CREATE PROCEDURE `update_into_motoristas`(in _ID int, in _Nome varchar(50),in _CartaConducao tinyint,in _Telefone tinyint)
 UPDATE `Transportadora`.`Motoristas`
@@ -26,11 +25,10 @@ SET
 WHERE `ID` = _ID;
 END$$
 DELIMITER ;
-
 CALL update_into_motoristas(3,'John Doe',000000001,000000001);
 
+-- Calcula o Total de Entregas Realizadas num determinado período
 DROP PROCEDURE IF EXISTS calculate_total_entregas_between;
-
 DELIMITER $$
 CREATE PROCEDURE `calculate_total_entregas_between`(in _InitialDate datetime,in _FinalDate datetime, OUT total INT)
 begin
@@ -41,8 +39,6 @@ BETWEEN _InitialDate
 AND _FinalDate;
 END$$
 DELIMITER ;
-
-
 SET @total_entregas = 0;
 CALL calculate_total_entregas_between("2024-07-15 14:20:00", "2025-07-15 14:20:00", @total_entregas);
 
